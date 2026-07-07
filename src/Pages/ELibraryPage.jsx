a6cea2664cf88162acff38e3675de2d435fb69fd
EGramPanchayat <anwit3171@gmail.com>
2026-07-07 12:47:39 +0530

    style: change book grid layout to 2 columns on mobile screens

--- src/Pages/ELibraryPage.jsx
+++ src/Pages/ELibraryPage.jsx
@@ -411,7 +411,7 @@ export default function ELibraryPage() {
               </div>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
               {paginatedBooks.map((book) => (
                 <div 
                   key={book._id} 
